d16806c4bab32ede95ae35fc3a1e54743363a436
Scott Ranger <millenniumnight@googlemail.com>
2019-01-11 21:45:41 +0000

    Updated to return task entities in queries

--- Taskboard.Queries/DTO/ListDTO.cs
+++ Taskboard.Queries/DTO/ListDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Taskboard.Queries.DTO
+﻿using System.Collections.Generic;
+
+namespace Taskboard.Queries.DTO
 {
     public class ListDTO
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public IEnumerable<TaskDTO> Tasks { get; set; }
     }
 }
---
+++ Taskboard.Queries/DTO/TaskDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskboard.Queries.DTO
+{
+    public class TaskDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
